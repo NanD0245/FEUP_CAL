19838b0933565eabe914d0aff170861c90e5d4f2
NanD0245 <fernandoluisrego@gmail.com>
2021-02-21 11:56:03 +0000

    remove files

--- TP1/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mim/Documents/FEUP/2ano/Cadeiras/2Semestre/CAL/FEUP_CAL/TP1/lib/googletest-master
-# Build directory: /home/mim/Documents/FEUP/2ano/Cadeiras/2Semestre/CAL/FEUP_CAL/TP1/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")

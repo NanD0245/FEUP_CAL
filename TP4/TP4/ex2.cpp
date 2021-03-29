99125ac4cc59a3d07c16819f8e8b59dfa8236d97
up201905951 <up201905951@fe.up.pt>
2021-03-29 15:38:28 +0100

    done TP5

--- TP4/TP4/ex2.cpp
+++ TP4/TP4/ex2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 bool changeMakingUnlimitedDP(unsigned int C[], unsigned int n, unsigned int T, unsigned int usedCoins[]) {
     // TODO
-    int min = 9999999;
+    /*int min = 9999999;
     int count = 0;
     unsigned int coins[n];
     bool check;
@@ -34,8 +34,39 @@ bool changeMakingUnlimitedDP(unsigned int C[], unsigned int n, unsigned int T, u
             usedCoins[i] = coins[i];
         }
     if (!check) return false;
-	return true;
+	return true;*/
 
+    int INF = T + 1;
+    int minCoins[T + 1];
+    int lastCoin[T + 1];
+
+    for (int i = 0; i <=T; i++)
+        lastCoin[i] = 0;
+
+    for (int i = 0; i < n; i++)
+        usedCoins[i] = 0;
+
+    minCoins[0] = 0;
+
+    for (int k = 1; k <= T; k++)
+        minCoins[k] = INF;
+
+    for (int i = 1; i <= n; i++)
+        for (int k = C[i - 1]; k <= T; k++)
+            if (minCoins[k - C[i - 1]] + 1 < minCoins[k]) {
+                minCoins[k] = 1 + minCoins[k - C[i - 1]];
+                lastCoin[k] = C[i - 1];
+            }
+
+    if (minCoins[T] == INF) return false;
+
+    for (int k = T; k > 0; k-= lastCoin[k]) {
+        for (int i = 0; i < n; i++)
+            if (C[i] == lastCoin[k])
+                usedCoins[i]++;
+    }
+
+    return true;
 }
 
 
